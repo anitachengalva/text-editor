00eb9323ea00dcb752ea4183ca1e99630e5c390e
Anita Chengalva <anita.chengalva@gmail.com>
2022-07-13 23:26:37 -0700

    logic for method that gets content from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,15 +16,25 @@ const initdb = async () =>
 export const putDb = async (content) => {
   console.error('putDb not implemented');
   console.log('PUT to the database');
-  const jateDb = await openDB("jate", 1);
-  const tx = jateDb.transaction("jate", "readwrite");
-  const store = tx.objectStore("jate");
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
   const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('Data sucessfully saved to the database', result);
-}
+};
 
 // Logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
 
 initdb();
